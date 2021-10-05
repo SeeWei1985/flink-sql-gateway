ffd3c784a0b20c8582e3aa3d776a337dad68f097
kanwei <hbrkanwei@163.com>
2021-10-05 15:22:54 +0800

    属性包括分区属性和普通属性

--- src/main/java/com/ververica/flink/table/gateway/catalog/MysqlCatalog.java
+++ src/main/java/com/ververica/flink/table/gateway/catalog/MysqlCatalog.java
@@ -122,7 +122,7 @@ public class MysqlCatalog extends AbstractCatalog {
                     "\tjoin biz_meta_db_tb_rel rl on db.id=rl.db_id\n" +
                     "\tJOIN biz_meta_table tb ON rl.tb_id = tb.id\n" +
                     "\tJOIN biz_meta_field cl ON tb.id = cl.tb_id \n" +
-                    "WHERE db.db_name =?  \tAND tb.tb_name =? and  cl.partition_flag=0");
+                    "WHERE db.db_name =?  \tAND tb.tb_name =?  ");
 
             ps.setString(1, databaseName);
             ps.setString(2, tableName);
